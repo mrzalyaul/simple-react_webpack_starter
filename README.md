6446cef5f08d7cc9ae209096d58fa3643dd6cc0d
mrzalyaul <mrzalyaul@gmail.com>
2020-08-19 22:59:47 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# simple-react_webpack_starter
+
+## Instructions
+
+Requirements : install [node.js](https://nodejs.org/en/) and npm.
+
+```sh
+
+# Clone the souce code
+git clone https://github.com/mrzalyaul/simple-react_webpack_starter.git
+
+# Install dependencies
+npm install
+
+# Serve on localhost:8080
+npm start
+
+# Build for production
+npm run build
+
+```
+
+## Creators
+
+    Mohammed Rabie ZALYAUL
+    mrzalyaul@gmail.com
+
+## License
+
+This project is open-sourced software licensed under the [MIT License](https://opensource.org/licenses/MIT)
---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "images",
+          },
+        },
+      },
+
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
